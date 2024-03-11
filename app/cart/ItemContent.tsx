8b526eb50cc4c26e306af49d46a11e71e2ccd743
HyunwookJung0827 <as9804@berkeley.edu>
2024-03-10 22:42:49 -0700

    Next13 Fullstack E-Commerce 🛒 - 32 Decrease Item Cart Quantity

--- app/cart/ItemContent.tsx
+++ app/cart/ItemContent.tsx
@@ -12,7 +12,11 @@ interface ItemContentProps {
   item: CartProductType;
 }
 const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
-  const { handleRemoveProductFromCart, handleCartQtyIncrease } = useCart();
+  const {
+    handleRemoveProductFromCart,
+    handleCartQtyIncrease,
+    handleCartQtyDecrease,
+  } = useCart();
   return (
     <div
       className="
@@ -66,7 +70,7 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
           cartCounter={true}
           cartProduct={item}
           handleQtyIncrease={() => handleCartQtyIncrease(item)}
-          handleQtyDecrease={() => {}}
+          handleQtyDecrease={() => handleCartQtyDecrease(item)}
         />
       </div>
       <div className="justify-self-end font-semibold">

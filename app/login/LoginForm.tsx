d0aca085b227c556d3c5ebd8558530ebe185c9d3
HyunwookJung0827 <as9804@berkeley.edu>
2024-03-11 02:29:57 -0700

    LoginForm

---
+++ app/login/LoginForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/Input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+
+// Client component because of interactivity
+const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Call useForm hook to get register and handleSubmit functions
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    // Pass some default values to the form
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="Sign in to E-Shop" />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={AiOutlineGoogle}
+        onClick={() => {}}
+      />
+      <hr
+        className="bg-slate-300
+    w-full
+    h-px"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Login"}
+        onClick={handleSubmit(onSubmit)}
+      />
+      <p>
+        Do not have an account?{" "}
+        <Link className="underline" href="/register">
+          Sign Up
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default LoginForm;

781aa4b0ec0a9c03ab1a6e0e972ef31cbf75e399
HyunwookJung0827 <as9804@berkeley.edu>
2024-03-10 22:19:20 -0700

    Next13 Fullstack E-Commerce 🛒 - 29 Cart Item Design

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { MdArrowBack } from "react-icons/md";
 import Heading from "../components/Heading";
 import Button from "../components/Button";
+import ItemContent from "./ItemContent";
 
 const CartClient = () => {
   const { cartProducts } = useCart();
@@ -46,7 +47,7 @@ const CartClient = () => {
       <div>
         {cartProducts &&
           cartProducts.map((item) => {
-            return <div key={item.id}>{item.name}</div>;
+            return <ItemContent key={item.id} item={item} />;
           })}
       </div>
       <div

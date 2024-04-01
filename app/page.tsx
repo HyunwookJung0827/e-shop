e6d1e8c5a04ad80dc446682788ebf96e1fa7c664
HyunwookJung0827 <as9804@berkeley.edu>
2024-03-31 18:21:12 -0700

    Product Categories but Phone Image load Error: Invalid src prop (https://firebasestorage.googleapis.com/v0/b/e-shop-vid-bcf8e.appspot.com/o/products%2F1711865149624-download.jpeg?alt=media&token=d3fbfb1f-0753-4951-8bbb-07a0dafa36d6) on `next/image`, hostname "firebasestorage.googleapis.com" is not configured under images in your `next.config.js` See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
-import { truncateText } from "@/utils/truncateText";
 import ProductCard from "./components/products/ProductCard";
+import getProducts, { IProductParams } from "@/actions/getProduct";
+import NullData from "./components/NullData";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: IProductParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
+
+  // We want to show the products in a random order
+  if (products.length === 0) {
+    return (
+      <NullData title='Oops! No products found. Click "All" to clear filters' />
+    );
+  }
+
+  // Fisher-Yates shuffle algorithm
+  function shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const shuffledProducts = shuffleArray(products);
+
   return (
     <div className="p-8">
       <Container>
@@ -17,8 +41,8 @@ export default function Home() {
         2xl:grid-cols-6
         gap-8"
         >
-          {products.map((product: any) => {
-            return <ProductCard data={product} />;
+          {shuffledProducts.map((product: any) => {
+            return <ProductCard key={product.id} data={product} />;
           })}
         </div>
       </Container>
